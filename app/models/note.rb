f9c6ab976e308003f9aaaa7f8373f9289df2a858
Benito Miranda <benomir82@gmail.com>
2015-09-07 00:14:59 -0500

    Add method to display share as text

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,8 @@ class Note < ActiveRecord::Base
 	validates :title, :content, :user_id, presence: true
 	validates_inclusion_of :share, in: [true, false]
 	belongs_to :user
+
+	def boolean_to_text
+		self.share ? 'Yes' : 'No'
+	end
 end
