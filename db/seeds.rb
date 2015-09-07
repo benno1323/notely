35b3415d88481f9b747749d6c47479efd5cc9096
Benito Miranda <benomir82@gmail.com>
2015-09-06 23:54:09 -0500

    Seed users and notes data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Note.destroy_all
+User.destroy_all
+
+5.times do |i|
+	user = User.create!(email: "user#{i}@gmail.com", password: "12345678", password_confirmation: "12345678")
+	2.times do |j|
+		Note.create!(title: "Note #{j}", content: "Content of Note #{j}", share: false, user: user)
+	end
+end
