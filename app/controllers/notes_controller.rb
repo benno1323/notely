c4fa2bbb0e89cae92598335abb7936c8b3af7cc4
Benito Miranda <benomir82@gmail.com>
2015-09-08 20:03:21 -0500

    Show notes with share value true in index

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,7 +5,7 @@ class NotesController < ApplicationController
   # GET /notes
   # GET /notes.json
   def index
-    @notes = Note.all
+    @notes = Note.where(share: true)
   end
 
   # GET /notes/1
