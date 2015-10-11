551d62daf0334348664dff6d016ebc50be83f9a9
Benito Miranda <benomir82@gmail.com>
2015-10-11 03:50:37 -0500

    Join notes and users table and bring records where share is true

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,7 +5,7 @@ class NotesController < ApplicationController
   # GET /notes
   # GET /notes.json
   def index
-    @notes = Note.where(share: true)
+    @notes = Note.joins(:user).where(share: true)
   end
 
   # GET /notes/1
