c58e71b3f03d4295c372e3ce7eca634deea7d48b
Benito Miranda <benomir82@gmail.com>
2015-10-25 21:16:14 -0600

    Add test and validate a comment has content and user id

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
+  validates :content, :user_id, presence: true
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@comment = comments(:one)
+  end
+
+  test "should have content" do
+  	@comment.content = nil
+  	assert_not @comment.save
+  end
+
+  test "should have user id" do
+  	@comment.user_id = nil
+  	assert_not @comment.save
+  end
 end
