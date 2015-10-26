90534e817b6aa4517ff37399ffea1c212765451a
Benito Miranda <benomir82@gmail.com>
2015-10-25 21:08:12 -0600

    Generate Comment Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026022040) do
+ActiveRecord::Schema.define(version: 20151026030713) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
