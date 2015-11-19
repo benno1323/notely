358006e53f0da73ea8399a28a8b0c4fba0af4df1
Benito Miranda <benomir82@gmail.com>
2015-11-18 20:18:25 -0600

    Update query to get last 20 notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,7 +5,7 @@ class NotesController < ApplicationController
   # GET /notes
   # GET /notes.json
   def index
-    @notes = Note.joins(:user).where(share: true)
+    @notes = Note.joins(:user).where(share: true).last(20)
   end
 
   # GET /notes/1
