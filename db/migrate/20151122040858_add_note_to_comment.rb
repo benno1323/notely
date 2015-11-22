27f932c24b1de99446d90f2a879e39961e0daf9a
Benito Miranda <benomir82@gmail.com>
2015-11-22 00:16:46 -0600

    Add note reference to comments table

---
+++ db/migrate/20151122040858_add_note_to_comment.rb
@@ -0,0 +1,5 @@
+class AddNoteToComment < ActiveRecord::Migration
+  def change
+    add_reference :comments, :note, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026030713) do
+ActiveRecord::Schema.define(version: 20151122040858) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "note_id"
   end
 
+  add_index "comments", ["note_id"], name: "index_comments_on_note_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "notes", force: :cascade do |t|
