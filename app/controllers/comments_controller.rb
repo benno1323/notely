daf699596b97dd0757813af85da592a91162f132
Benito Miranda <benomir82@gmail.com>
2015-11-27 22:17:58 -0600

    Add create comment method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,28 @@
 class CommentsController < ApplicationController
+	before_action :set_note, only: :create
 	before_action :authenticate_user!, except: :show
 
-	def index
-
+	def create
+		build_comment
+		if @comment.save
+			redirect_to @note
+		else
+			render 'notes/show'
+		end
 	end
+
+	private
+
+		def set_note
+			@note = Note.find(params[:note_id])
+		end
+
+		def build_comment
+			@comment = @note.comments.build(comment_params)
+			@comment.user = current_user
+		end
+
+		def comment_params
+			params.require(:comment).permit(:content)
+		end
 end
