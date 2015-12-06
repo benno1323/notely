0f2d17529659968fe64fdb760d99b119f0370398
Benito Miranda <benomir82@gmail.com>
2015-12-06 14:30:28 -0600

    Improve spec using factories

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,41 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-	describe "POST #create" do
-		context "with valid attributes" do
+	describe 'POST #create' do
+		context 'with valid attributes' do
 			before(:each) do
-				@user = create(:user)
+				user = create(:user)
 				@note = create(:note)
-				sign_in @user
+				sign_in user
 			end
 
 			it 'saves a comment in the database' do
 				expect{
 					post :create, note_id: @note.id,
-					comment: { content: 'My comment', note_id: @note.id, user_id: @user.id }
+						comment: attributes_for(:comment)
 					}.to change(Comment, :count).by(1)
 				end
 
-				it "redirect to notes#show" do
+				it 'redirect to notes#show' do
 					post :create, note_id: @note.id,
-					comment: { content: 'My comment', note_id: @note.id, user_id: @user.id }
-					expect(response).to redirect_to(note_path(@note))
+					comment: attributes_for(:comment)
+					expect(response).to redirect_to(@note)
 				end
 			end
 		end
 
-		context "with invalid attributes" do
+		context 'with invalid attributes' do
 			before(:each) do
-				@user = create(:user)
+				user = create(:user)
 				@note = create(:note)
-				sign_in @user
+				sign_in user
 			end
 
 			it 're-renders notes#show' do
-				invalid_attributes = { content: '', note_id: @note.id, user_id: @user.id }
 				post :create, note_id: @note.id,
-				comment: invalid_attributes
+				comment: attributes_for(:comment, content: '')
 				expect(response).to render_template(:show)
+			end
+		end
+
+		context 'when user is not signed in' do
+			it 'redirects to users#sign_in' do
+				@note = create(:note)
+				post :create, note_id: @note.id,
+					comment: attributes_for(:comment)
+					expect(response).to redirect_to(new_user_session_path)
 			end
 		end
 	end
