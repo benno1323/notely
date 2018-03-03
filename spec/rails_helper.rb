cae8c49ca793a4cbd74591d5944fb71d4b05af33
Benito Miranda <benomir82@gmail.com>
2018-03-02 23:43:30 -0600

    Setup database_cleaner and FactoryBot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/rspec'
+require 'support/factory_bot'
+require 'support/database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -33,7 +36,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -49,9 +52,6 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
-  # Include Factory Girl syntax to simplify calls to factories
-  config.include FactoryBot::Syntax::Methods
 
   config.include Devise::TestHelpers, type: :controller
 end
---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,4 @@
+RSpec.configure do |config|
+  # Include Factory Bot syntax to simplify calls to factories
+  config.include FactoryBot::Syntax::Methods
+end
